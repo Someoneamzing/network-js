c773f6630e2aa5be1b51582f49c7d27f790752e6
Jacob McEwan <jacob@mchew.com>
2020-12-14 00:41:45 +1100

    Now pass through open, close and error events

--- SocketClientBrowser.mjs
+++ SocketClientBrowser.mjs
@@ -9,11 +9,14 @@ export default class Client extends Common {
     console.log(this.url.href);
     this.ws = new WebSocket(this.url.href);
     this.ws.binaryType = 'arraybuffer';
-    this.ws.addEventListener('open', ()=>{
+    this.ws.addEventListener('open', (...args)=>{
       console.log("WS connected");
+      this.emit('open', ...args);
     })
     this.handler = this.messageHandler(this.ws);
     this.ws.addEventListener('message', (event)=> this.handler(event.data))
+    this.ws.addEventListener('error', (...args)=>this.emit('error', ...args));
+    this.ws.addEventListener('close', (...args)=>this.emit('close', ...args));
   }
 
   send(...args) {
