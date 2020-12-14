edd11dafe9b1357d7bf102fb1cf50690034855a1
Jacob McEwan <jacob@mchew.com>
2020-12-14 22:39:46 +1100

    Fixed id generation bug

--- SocketCommon.mjs
+++ SocketCommon.mjs
@@ -3,7 +3,7 @@ import Message from './Message.mjs';
 export const ALPHABET = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 export function generateID() {
-    return Array.from({length: 25}, _=>ALPHABET[(Math.random() * ALPHABET.length) | 0])
+    return Array.from({length: 25}, _=>ALPHABET[(Math.random() * ALPHABET.length) | 0]).join('');
 }
 
 export default class Common {
